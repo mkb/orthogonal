a28da514cd7ec26b589614155aaa5402fef111c3
Michael Kennedy Brodhead <mkb@orthogonal.org>
2011-05-19 20:06:43 -0700

    united colors of jekyll

--- _tasks/validate.rb
+++ _tasks/validate.rb
@@ -27,17 +27,19 @@ namespace :validate do
   end
 end
 
-
 private
 
 # Colorize the output :)
 def colorize(text, color_code); "#{color_code}#{text}\e[0m"; end
 def red(text); colorize(text, "\e[31m"); end
 def green(text); colorize(text, "\e[32m"); end
+def yellow(text); colorize(text, "\e[33m"); end
+def blue(text); colorize(text, "\e[34m"); end
+def purple(text); colorize(text, "\e[35m"); end
 def cyan(text); colorize(text, "\e[36m"); end
+def cracker(text); colorize(text, "\e[37m"); end
 
 
-# Method to validate calling to the w3c_validators methods
 def validate_type(ext)
   files(@config['destination'], ext).each do |file|
     results = validate_file(file)
